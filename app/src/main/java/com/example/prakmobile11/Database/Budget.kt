9add196bfdf936ed3014962c676a535237297799
Arviansyah Eka <arviansayhekapk@gmail.com>
2023-11-21 08:47:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/prakmobile11/Database/Budget.kt
@@ -0,0 +1,9 @@
+package com.example.prakmobile11.Database
+
+data class Budget(
+    var id: String = "",
+    var nominal: String = "",
+    var description: String = "",
+    var date: String = ""
+)
+
